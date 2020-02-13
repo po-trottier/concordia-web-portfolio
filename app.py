5d4888cd582551f5781f1a76793a638c75001af3
Pierre-Olivier Trottier <po.trottier@gmail.com>
2020-02-13 15:37:02 -0500

    Modified Template to Follow Requirements

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+# Initialize app
+app = Flask(__name__)
+
+# Configuration
+app.config.from_object('config')
+
+
+# Home Page
+@app.route('/')
+def home():
+  return render_template('routes/index.html')
+
+
+app.run(host='127.0.0.1', port=5000, debug=True)
--- run.py
+++
@@ -1,3 +0,0 @@
-# Run a test server.
-from server import app
-app.run(host='127.0.0.1', port=5000, debug=True)
