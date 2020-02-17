4fee6f9980bb85249414f34e7a798ff2fa42f35b
Pierre-Olivier Trottier <po.trottier@gmail.com>
2020-02-17 17:08:08 -0500

    Fixed Contact Form + Added Python Logic to Catch Info

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Initialize app
 app = Flask(__name__,
@@ -10,11 +10,24 @@ app = Flask(__name__,
 app.config.from_object('config')
 
 # Define the routes
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-  return render_template('routes/index.html')
+  # If we receive a POST request then it's a form submission
+  if request.method == "POST":
+    # If the anti-bot field is filled, a bot filled the form
+    if request.form.get('subject') != '':
+      return 'Bot Detected'
+    # Otherwise deal with the form
+    else:
+      print(request.form)
+      return render_template('routes/index.html')
+  # Return the index.html file for any other type of request
+  else:
+    return render_template('routes/index.html')
+
 @app.route('/blog')
 def blog():
+  # Will need to add logic here
   return render_template('routes/blog.html')
 
 app.run(host='127.0.0.1', port=5000, debug=True)
